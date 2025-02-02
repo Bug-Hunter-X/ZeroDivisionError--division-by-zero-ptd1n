88e1eb4a0012424307e31b2022eec4ae19502b6e
BugHunter-X <askzenai@gmail.com>
2025-02-02 04:48:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,8 @@
+def function(a, b):
+    if b == 0:
+        return 0  # Or raise an exception: raise ZeroDivisionError("Division by zero")
+    else:
+        return a / b
+
+result = function(10, 0)
+print(result) # Output: 0 
